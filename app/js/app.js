bd2474e71ac4153550be9b109c2c634f3850e9a4
vmnekrasovv <vmnekrasov777@gmail.com>
2024-09-23 23:39:09 +0300

    Фикс window load/resize

--- app/js/app.js
+++ app/js/app.js
@@ -5,17 +5,18 @@
 			$('.other-menu').toggle();
 		});
 
-		$(window).on('load resize', function(){
-			
-			let windowWidth = $(window).width();
-			let otherMenu = $('.other-menu');
-
-			if(windowWidth < 769){
-				otherMenu.appendTo('.header-menu-wrapper');
-			} else {
-				otherMenu.appendTo('.other-menu-wrapper');
-			}
-		});
-
 	});
+
+	$(window).on('load resize', function(){
+			
+		let windowWidth = $(window).width();
+		let otherMenu = $('.other-menu');
+
+		if(windowWidth < 769){
+			otherMenu.appendTo('.header-menu-wrapper');
+		} else {
+			otherMenu.appendTo('.other-menu-wrapper');
+		}
+	});
+	
 })(jQuery);
