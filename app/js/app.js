c3a50787d769c059ea188821aef348ce7ecea1cb
vmnekrasovv <vmnekrasov777@gmail.com>
2024-09-24 19:07:34 +0300

    первая секция адаптив

--- app/js/app.js
+++ app/js/app.js
@@ -9,20 +9,34 @@
 			}
 		});
 
-	});
+	}); // document.ready
+	
 
 	$(window).on('load resize', function(){
 			
-		let windowWidth = $(window).width();
+		let window_width = $(window).width();
+	
+	
+	//	otherMenu
+
 		let otherMenu = $('.other-menu');
 
-		if(windowWidth < 769){
+		if(window_width < 769){
 			otherMenu.appendTo('.header-menu-wrapper');
 			$('.other-menu').css({'display': 'flex'});
 		} else {
 			otherMenu.appendTo('.other-menu-wrapper');
 			$('.other-menu').css({'display': 'none'});
 		}
+
+	// serversItem
+
+		let serversItem = $('.servers-item');
+		let serversItem_width = serversItem.width();
+
+		serversItem.height(serversItem_width);
+
+
 	});
 
 })(jQuery);
