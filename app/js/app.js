2f0df9302badab5b7ea00a6bf259c02393ceec6d
vmnekrasovv <vmnekrasov777@gmail.com>
2024-09-27 19:09:49 +0300

    вынес переменные в начало, разделил document/window

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,13 @@
 (function($){
 
+	const headerMenuWrapper = $('.header-menu-wrapper');
+	const footerSecondLine = $('.footer__second-line');
+
+	const headerMenu = $('.header-menu');
+	const otherMenu = $('.other-menu');
+	const termsPrivacy = $('.terms-privacy')
+	const otherMenuWrapper = $('.other-menu-wrapper');
+
 
 	$(document).ready(function(){ 
 
@@ -40,14 +48,6 @@
 			$(this).removeClass('active');
 		});
 
-		const headerMenuWrapper = $('.header-menu-wrapper');
-		const footerSecondLine = $('.footer__second-line');
-
-		const headerMenu = $('.header-menu');
-		const otherMenu = $('.other-menu');
-		const termsPrivacy = $('.terms-privacy')
-		const otherMenuWrapper = $('.other-menu-wrapper');
-
 
 		$('.burger').on('click', function(){
 
@@ -58,39 +58,38 @@
 			}
 		});
 
-
-		$(window).on('load resize', function(){
-	
-			let window_width = window.innerWidth;
-
-		// mergeMenu
-
-			if(window_width < 769){
-				headerMenu.after(otherMenu);
-				otherMenu.css({'display': 'flex'});
-
-				console.log('window_width < 769', otherMenu);
-
-				if(headerMenuWrapper.hasClass('active')){
-					termsPrivacy.appendTo(headerMenuWrapper);
-				}
-			
-			} 
-			else {
-				otherMenu.appendTo(otherMenuWrapper);
-				otherMenu.css({'display': 'none'});
-				termsPrivacy.appendTo(footerSecondLine);
-			}
-
-			// serversItem setHeight
-
-			setImgHeight();
-
-
-		}); // window load/resize
-
-
 	}); // document.ready
+
+	$(window).on('load resize', function(){
+	
+		let window_width = window.innerWidth;
+
+	// mergeMenu
+
+		if(window_width < 769){
+			
+			headerMenu.after(otherMenu);
+			otherMenu.css({'display': 'flex'});
+
+			console.log('window_width < 769', otherMenu);
+
+			if(headerMenuWrapper.hasClass('active')){
+				termsPrivacy.appendTo(headerMenuWrapper);
+			}
+		
+		} 
+		else {
+			otherMenu.appendTo(otherMenuWrapper);
+			otherMenu.css({'display': 'none'});
+			termsPrivacy.appendTo(footerSecondLine);
+		}
+
+		// serversItem setHeight
+
+		setImgHeight();
+
+
+	}); // window load/resize
 
 
 })(jQuery);
