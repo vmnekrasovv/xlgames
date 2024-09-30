29057ee7641016ca529693e4a809e7a6c149c47c
vmnekrasovv <vmnekrasov777@gmail.com>
2024-09-30 07:44:43 +0300

    рефак бургер багфикс

--- app/js/burger.js
+++ app/js/burger.js
@@ -1,28 +1,29 @@
 (function($){
 
-	$(window).on('load', function(){
-		
-		let wW = window.innerWidth;
+	let header = $('.header');
+	let headerMenuWrapper = $('.header-menu-wrapper');
+	let burger = $('.burger');
 
-		$('.burger').append("<span class='burger__element'></span>");
+	$(document).ready(function(){
 
-		let btn = $('.burger, .header-menu-wrapper');
-
-		btn.click(function(){
-			if(wW < 769){
-				btn.toggleClass('active');
-				$('.header').toggleClass('active');
-				$('body').toggleClass('lock');
-			}
+		burger.on('click', function(e){
+			burger.toggleClass('active');
+			header.toggleClass('active');
+			headerMenuWrapper.toggleClass('active');
+			$('body').toggleClass('lock');
 		});
 	});
 
-	$(window).on('resize', function(){
-		if(window.innerWidth > 768){
-			$('.burger').removeClass('active');
-			$('.header').removeClass('active');
-			$('.header-menu-wrapper').removeClass('active');
+	$(window).on('load resize', function(){
+
+		wW = window.innerWidth;
+
+		if(wW > 768){
+			burger.removeClass('active');
+			header.removeClass('active');
+			headerMenuWrapper.removeClass('active');
 			$('body').removeClass('lock');
 		}
 	});
+
 })(jQuery);
